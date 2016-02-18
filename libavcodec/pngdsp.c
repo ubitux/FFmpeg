37ad40ca08d8033ac0fa197736312298288850f2
Clément Bœsch <clement@stupeflix.com>
2016-02-18 11:21:11 +0100

    oui

--- libavcodec/pngdsp.c
+++ libavcodec/pngdsp.c
@@ -30,14 +30,12 @@
 
 static void add_bytes_l2_c(uint8_t *dst, uint8_t *src1, uint8_t *src2, int w)
 {
-    long i;
-    for (i = 0; i <= w - (int) sizeof(long); i += sizeof(long)) {
+    int i;
+    for (i = 0; i < w; i++) {
         long a = *(long *)(src1 + i);
         long b = *(long *)(src2 + i);
         *(long *)(dst + i) = ((a & pb_7f) + (b & pb_7f)) ^ ((a ^ b) & pb_80);
     }
-    for (; i < w; i++)
-        dst[i] = src1[i] + src2[i];
 }
 
 static void add_png_paeth_prediction_c(uint8_t *dst, uint8_t *src, uint8_t *top,
